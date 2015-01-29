d110614e09c70aa5858db35767ab421f3d3b02b3
Bogdan Sala <bogdansala@doru-cailean.skobbler.local>
2015-01-29 17:54:52 +0200

    Initial Commit

---
+++ game/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  game
+//
+//  Created by Bogdan Sala on 29/01/15.
+//  Copyright (c) 2015 Telenav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
