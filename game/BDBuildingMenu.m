9983597cae841372917f43504a7be3026d7dc080
salabogdan <sala_bogdan@yahoo.com>
2015-03-30 18:51:21 +0300

    Added a new project folder for all json files. Changed the initialisation method for the menu details to be appropriate.

--- game/BDBuildingMenu.m
+++ game/BDBuildingMenu.m
@@ -18,61 +18,71 @@
 @end
 
 @implementation BDBuildingMenu
-//Review:SB plase use cammelCase for all names
-//please use one line for one of only  a variable
+
 - (instancetype)initWithBuilding:(BDBuilding *)building andFrame:(CGRect)frame{
     self = [super initWithFrame:frame];
     if(self) {
         self.building = building;
-        UIView *container;
-        UIButton *exitButton;
-        exitButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 30, 30)];
+       
+        UIButton *exitButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 30, 30)];
         [exitButton setTitle:@"X" forState:UIControlStateNormal];
         [exitButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-//        exitButton.backgroundColor = [UIColor whiteColor];
         [exitButton addTarget:self action:@selector(didTouchExitButton) forControlEvents:UIControlEventTouchUpInside];
+
         self.icon = [[UIImageView alloc] initWithFrame:CGRectMake(50, 50, 200, 160)];
         self.icon.backgroundColor = [UIColor blueColor];
-        UIImageView *TimeIcon;
-        UILabel *GoldLabel, *WoodLabel, *IronLabel, *PeopleLabel, *GoldValue, *WoodValue, *IronValue, *TimeValue, *PeopleValue;
-        CGFloat a = CGRectGetMaxX(self.icon.frame);
-        GoldLabel = [[UILabel alloc] initWithFrame:CGRectMake(a + 15, self.icon.frame.origin.y, 60, self.icon.frame.size.height / 4)];
-        GoldLabel.text = [NSString stringWithFormat:@"Gold"];
-        WoodLabel = [[UILabel alloc] initWithFrame:CGRectMake(a + 15, CGRectGetMaxY(GoldLabel.frame), 60, self.icon.frame.size.height / 4)];
-        WoodLabel.text = [NSString stringWithFormat:@"Wood"];
-        IronLabel = [[UILabel alloc] initWithFrame:CGRectMake(a + 15, CGRectGetMaxY(WoodLabel.frame), 60, self.icon.frame.size.height / 4)];
-        IronLabel.text = [NSString stringWithFormat:@"Iron"];
-        PeopleLabel = [[UILabel alloc] initWithFrame:CGRectMake(a + 15, CGRectGetMaxY(IronLabel.frame), 60, self.icon.frame.size.height / 4)];
-        PeopleLabel.text = [NSString stringWithFormat:@"People"];
+        
+        CGFloat buttonsX = CGRectGetMaxX(self.icon.frame) + 15;
+        UILabel *goldLabel = [[UILabel alloc] initWithFrame:CGRectMake(buttonsX, self.icon.frame.origin.y, 60, self.icon.frame.size.height / 4)];
+        goldLabel.text = [NSString stringWithFormat:@"Gold"];
+        UILabel *woodLabel = [[UILabel alloc] initWithFrame:CGRectMake(buttonsX, CGRectGetMaxY(goldLabel.frame), 60, self.icon.frame.size.height / 4)];
+        woodLabel.text = [NSString stringWithFormat:@"Wood"];
+        UILabel *ironLabel = [[UILabel alloc] initWithFrame:CGRectMake(buttonsX, CGRectGetMaxY(woodLabel.frame), 60, self.icon.frame.size.height / 4)];
+        ironLabel.text = [NSString stringWithFormat:@"Iron"];
+        UILabel *peopleLabel = [[UILabel alloc] initWithFrame:CGRectMake(buttonsX, CGRectGetMaxY(ironLabel.frame), 60, self.icon.frame.size.height / 4)];
+        peopleLabel.text = [NSString stringWithFormat:@"People"];
+
+        buttonsX = CGRectGetMaxX(goldLabel.frame);
+        UILabel *goldValue = [[UILabel alloc] initWithFrame:CGRectMake(buttonsX, goldLabel.frame.origin.y, 60, self.icon.frame.size.height / 4)];
+        goldValue.text = [NSString stringWithFormat:@"1000"];
+        UILabel *woodValue = [[UILabel alloc] initWithFrame:CGRectMake(buttonsX, woodLabel.frame.origin.y, 60, self.icon.frame.size.height / 4)];
+        woodValue.text = [NSString stringWithFormat:@"1500"];
+        UILabel *ironValue = [[UILabel alloc] initWithFrame:CGRectMake(buttonsX, ironLabel.frame.origin.y, 60, self.icon.frame.size.height / 4)];
+        ironValue.text = [NSString stringWithFormat:@"800"];
+        UILabel *peopleValue = [[UILabel alloc] initWithFrame:CGRectMake(buttonsX, peopleLabel.frame.origin.y, 60, self.icon.frame.size.height / 4)];
+        peopleValue.text = [NSString stringWithFormat:@"5"];
+
         CGFloat originY = CGRectGetMaxY(self.icon.frame) + 20;
-        container = [[UIView alloc] initWithFrame:CGRectMake(self.icon.frame.origin.x, originY, self.frame.size.width - 100, self.frame.size.height - originY - 50)];
+        UIView *container = [[UIView alloc] initWithFrame:CGRectMake(self.icon.frame.origin.x, originY, self.frame.size.width - 100, self.frame.size.height - originY - 50)];
         container.backgroundColor = [UIColor greenColor];
-        CGFloat b = (self.frame.size.width - container.frame.size.width) / 2;
-        self.upgrade = [[UIButton alloc] initWithFrame:CGRectMake(self.frame.size.width - b - 180, self.icon.frame.origin.y, 180, 75)];
+        
+        CGFloat upgradeButtonX = (self.frame.size.width - container.frame.size.width) / 2;
+        self.upgrade = [[UIButton alloc] initWithFrame:CGRectMake(self.frame.size.width - upgradeButtonX - 180, self.icon.frame.origin.y, 180, 75)];
         [self.upgrade setTitle:@"Upgrade" forState:UIControlStateNormal];
         self.upgrade.backgroundColor =[UIColor blueColor];
         [self.upgrade addTarget:self action:@selector(upgradeBuilding) forControlEvents:UIControlEventTouchUpInside];
-        UIImage *timeImage;
-        timeImage = [UIImage imageNamed:@"TimeIcon"];
-        TimeIcon = [[UIImageView alloc] initWithFrame:CGRectMake(self.upgrade.frame.origin.x, CGRectGetMaxY(self.upgrade.frame) + 20, self.upgrade.frame.size.width / 3, self.upgrade.frame.size.width / 3)];
-        TimeIcon.image = timeImage;
-        TimeValue = [[UILabel alloc] initWithFrame:CGRectMake(CGRectGetMaxX(TimeIcon.frame) + 5, TimeIcon.frame.origin.y, 2 * TimeIcon.frame.size.width, TimeIcon.frame.size.height)];
-        TimeValue.text = [NSString stringWithFormat:@"2 H"];
+        
+        UIImage *timeImage = [UIImage imageNamed:@"timeIcon"];
+        UIImageView *timeIcon = [[UIImageView alloc] initWithFrame:CGRectMake(self.upgrade.frame.origin.x, CGRectGetMaxY(self.upgrade.frame) + 20, self.upgrade.frame.size.width / 3, self.upgrade.frame.size.width / 3)];
+        timeIcon.image = timeImage;
+        
+        UILabel *timeValue = [[UILabel alloc] initWithFrame:CGRectMake(CGRectGetMaxX(timeIcon.frame) + 5, timeIcon.frame.origin.y, 2 * timeIcon.frame.size.width, timeIcon.frame.size.height)];
+        timeValue.text = [NSString stringWithFormat:@"2 H"];
+        
         [self addSubview:self.icon];
         [self addSubview:self.upgrade];
         [self addSubview:container];
-        [self addSubview:GoldLabel];
-        [self addSubview:WoodLabel];
-        [self addSubview:IronLabel];
-        [self addSubview:PeopleLabel];
-        [self addSubview:TimeIcon];
-        [self addSubview:TimeValue];
-        [self addSubview:GoldValue];
-        [self addSubview:WoodValue];
-        [self addSubview:IronValue];
-        [self addSubview:PeopleValue];
+        [self addSubview:goldLabel];
+        [self addSubview:woodLabel];
+        [self addSubview:ironLabel];
+        [self addSubview:peopleLabel];
+        [self addSubview:timeIcon];
+        [self addSubview:timeValue];
+        [self addSubview:goldValue];
+        [self addSubview:woodValue];
+        [self addSubview:ironValue];
+        [self addSubview:peopleValue];
         [self addSubview:exitButton];
-        
     }
     return self;
 }
@@ -84,4 +94,5 @@
 - (void)upgradeBuilding{
     //
 }
+
 @end
